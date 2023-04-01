6bf85b13aeac8bfe2a9dde41b48e9443d8b07771
tamer <tamer.fakho@gmail.com>
2023-04-02 00:31:57 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import RefundPolicyPage from "./pages/RefundPolicyPage";
 
 function App() {
   
-  const url = 'https://market-api-od3p.onrender.com:10000/'
+  const url = 'https://market-api-od3p.onrender.com/'
   return(
     <div>
       <Routes>
