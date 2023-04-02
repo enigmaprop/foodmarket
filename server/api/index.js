3cea11356ead60b8cb7634d1543a57cd2a981f6f
tamer <tamer.fakho@gmail.com>
2023-04-02 13:14:43 +0300

    update

--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 mongoose.connect(process.env.DBURL);
 
@@ -46,7 +47,11 @@ app.use(cors({
   }
 }));
 
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const productRoutes = require('./routes/productRoutes');
 app.use('/products', productRoutes);
